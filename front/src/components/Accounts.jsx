a3c77926bd34ed12178c4fd29b0489c6659e8444
Your Name <sophia.ogg45@hotmail.com>
2024-06-22 16:03:36 +0200

    modifications de la page Profile.jsx

---
+++ front/src/components/Accounts.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Accounts = ({ accounts }) => {
+  return (
+    <>
+      {accounts.map((account, index) => (
+        <section className="account" key={index}>
+          <div className="account-content-wrapper">
+            <h3 className="account-title">{account.title}</h3>
+            <p className="account-amount">{account.amount}</p>
+            <p className="account-amount-description">{account.description}</p>
+          </div>
+          <div className="account-content-wrapper cta">
+            <button className="transaction-button">View transactions</button>
+          </div>
+        </section>
+      ))}
+    </>
+  );
+};
+
+Accounts.propTypes = {
+  accounts: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default Accounts;
--- front/src/pages/Profile.jsx
+++ front/src/pages/Profile.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom'; // Importer Link depuis react-router-dom
+import { Link } from 'react-router-dom';
+import Accounts from '../components/Accounts';
+import accountsData from '../data/accounts.json'; // Importer les données des comptes
 
 const Profile = () => {
   const user = useSelector((state) => state.auth.user);
@@ -10,44 +12,13 @@ const Profile = () => {
       <main className="main bg-dark">
         <div className="header">
           <h1>Welcome back<br />{user ? `${user.firstName} ${user.lastName}` : 'User'}!</h1>
-          {/* Utilisation de Link pour rediriger vers EditUser */}
           <Link to="/edit-user" className="edit-button">Edit Name</Link>
         </div>
         <h2 className="sr-only">Accounts</h2>
-        <section className="account">
-          <div className="account-content-wrapper">
-            <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-            <p className="account-amount">$2,082.79</p>
-            <p className="account-amount-description">Available Balance</p>
-          </div>
-          <div className="account-content-wrapper cta">
-            <button className="transaction-button">View transactions</button>
-          </div>
-        </section>
-        <section className="account">
-          <div className="account-content-wrapper">
-            <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-            <p className="account-amount">$10,928.42</p>
-            <p className="account-amount-description">Available Balance</p>
-          </div>
-          <div className="account-content-wrapper cta">
-            <button className="transaction-button">View transactions</button>
-          </div>
-        </section>
-        <section className="account">
-          <div className="account-content-wrapper">
-            <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-            <p className="account-amount">$184.30</p>
-            <p className="account-amount-description">Current Balance</p>
-          </div>
-          <div className="account-content-wrapper cta">
-            <button className="transaction-button">View transactions</button>
-          </div>
-        </section>
+        <Accounts accounts={accountsData} />
       </main>
     </div>
   );
 };
 
 export default Profile;
-
