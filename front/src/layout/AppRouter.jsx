0036b29d6c47bd4aca1baf3dfbb45bc5a2641c87
Your Name <sophia.ogg45@hotmail.com>
2024-06-24 12:22:16 +0200

    mis en place de PageLayout

--- front/src/layout/AppRouter.jsx
+++ front/src/layout/AppRouter.jsx
@@ -1,24 +1,26 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
 import Profile from '../pages/Profile';
 import PrivateRoute from '../redux/PrivateRoute';
-import EditUser from '../components/EditUser'; 
+import EditUser from '../components/EditUser';
+import PageLayout from "../layout/PageLayout";
+
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Header />
+
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/profile" element={<PrivateRoute element={<Profile />} />} />
-          <Route path="/edit-user" component={EditUser} />
+          <Route path="/" element={<PageLayout />} >
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<PrivateRoute element={<Profile />} />} />
+            <Route path="/edit-user" component={EditUser} />
+          </Route>
         </Routes>
-        <Footer />
+
       </div>
     </BrowserRouter>
   );
